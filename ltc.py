c350a06e47969e3cbdef8da401925e2d4854aee6
Matroftt <91145555+Matroftt@users.noreply.github.com>
2024-04-29 20:01:39 +0300

    finish rect and webdriver torso update
    Adds temporary randomly generated levels, new debug keys and level counter
    Also rects wont work on new levels except finish rect

--- ltc.py
+++ ltc.py
@@ -1,8 +1,16 @@
 '''
 list of some abbreviations in this code
 ln - level number
+sl - spawn location
+fl - finish location
 kb - killblock/killbrick
 plr - player
+
+list of hotkeys
+space - pause
+arrow up - previous level
+arrow down - next level
+arrow left - moves character up and left
 '''
 
 
@@ -11,7 +19,7 @@ WIDTH, HEIGHT = 1200, 700
 plr_img = pg.image.load('icon.png')
 cursor = pg.Rect(0,0,1,1)
 
-print('Only existing levels yet are 0 and 1')
+print('Only existing levels yet are 0-2 and 3-903 that are randomly generated')
 lvl = input('Go to level... ')
 if lvl == '':
     lvl = 0
@@ -21,10 +29,11 @@ class Player:
     def __init__(self, game):
         self.game = game
         self.paused = False
-        self.plr = pg.Rect(100,110,18,18)
+        self.level = Level(self) 
+        self.plr = pg.Rect(0,0,18,18)
         self.cap = 0
         self.mouse_x,self.mouse_y = pg.mouse.get_pos()
-        self.level = Level(self) 
+        
     def instance(self):
         if cursor.colliderect(self.plr):
             if self.game.paused == 0:
@@ -46,30 +55,74 @@ class Player:
         for i in range(len(self.level.lvl[self.level.ln])):
             self.kb = pg.Rect(self.level.lvl[self.level.ln][i][0], self.level.lvl[self.level.ln][i][1], self.level.lvl[self.level.ln][i][2], self.level.lvl[self.level.ln][i][3])
             if self.plr.colliderect(self.kb):
-                print("!")
+                self.reset()
+    def check_win(self):
+        if self.plr.colliderect(self.game.finish.rect):
+            print("Completed")
+            self.reset()
+            self.game.level.ln += 1
+      
+    def reset(self):
+        self.plr.left = self.level.sl[self.level.ln][0]
+        self.plr.top = self.level.sl[self.level.ln][1]
+        
 class Level:
     def __init__(self, game, ln=lvl):
         self.game = game
-        self.ln = ln # ln = level number
+        self.ln = ln 
         self.lvl = [
                         [
                          [0,0,WIDTH,HEIGHT/14],[0,HEIGHT-HEIGHT/14+1,WIDTH,HEIGHT/14],
                          [0,0,WIDTH/20,HEIGHT],[WIDTH-WIDTH/20+1,0,WIDTH/20,HEIGHT]   # Resizable frame
                         ],
                         
-                        [[500,500,50,50]]
+                        [[500,500,50,50]],
+                        
+                        [
+                            [300,300,20,20], [240,240,20,20] 
+                        ]
                    ]
-    def run(self,ln=1):
+        
+        self.sl = [
+                    [WIDTH/2, HEIGHT/2],
+                    [WIDTH/1.5, HEIGHT/3],
+                    [WIDTH/1.5, HEIGHT/3]
+                    
+                  ]
+        self.fl = [
+                    [WIDTH/10, HEIGHT/10, WIDTH/11, HEIGHT/11],
+                    [100,100,10,10],
+                    [0,0,85,80]
+                  ]
+        for i in range(900):
+            self.lvl.append([[random.randint(0,WIDTH),random.randint(0,HEIGHT),random.randint(0,100),random.randint(0,100)],
+                             [random.randint(0,WIDTH),random.randint(0,HEIGHT),random.randint(10,400),random.randint(10,400)],
+                             [random.randint(0,WIDTH),random.randint(0,HEIGHT),random.randint(50,500),random.randint(50,500)]])
+            self.sl.append([random.randint(0,WIDTH),random.randint(0,HEIGHT),random.randint(0,100),random.randint(0,100)])
+            self.fl.append([random.randint(0,WIDTH),random.randint(0,HEIGHT),random.randint(0,100),random.randint(0,100)])
+            
+        self.game.finish = Obstacle(self.game, self.fl[self.ln][0], self.fl[self.ln][1], self.fl[self.ln][2], self.fl[self.ln][3], type="finish")
+    def run(self):
         for i in range(len(self.lvl[self.ln])):
             self.game.block = Obstacle(self.game, self.lvl[self.ln][i][0], self.lvl[self.ln][i][1], self.lvl[self.ln][i][2], self.lvl[self.ln][i][3])
             self.game.block.blit()
+            self.game.finish = Obstacle(self.game, self.fl[self.ln][0], self.fl[self.ln][1], self.fl[self.ln][2], self.fl[self.ln][3], type="finish")
+            self.game.finish.blit()
+        numfont = pg.font.SysFont("Courier new", 50)
+        numtext = numfont.render(str(self.ln),0,(100,100,100))
+        self.game.app.sc.blit(numtext,(WIDTH//10,HEIGHT//5-50))
 class Obstacle:
-    def __init__(self, game, l=10, t=10, w=100, h=100, id=0, type="static"):
+    def __init__(self, game, l=10, t=10, w=100, h=100, color=(0,0,0), type="kb"):
         self.game = game
+        self.color = color
+        self.type = type
         self.rect = pg.Rect(l,t,w,h)
-        #print(l,t,w,h,id)
+        
+        if self.type == "finish" and self.color == (0,0,0):
+            self.color = (100,5,5)
+        
     def blit(self):
-        pg.draw.rect(self.game.app.sc, (0,0,0), self.rect)
+        pg.draw.rect(self.game.app.sc, self.color, self.rect)
         
 class Game:
     def __init__(self, app):
@@ -88,6 +141,7 @@ class Game:
         self.background()
         self.player.instance()
         self.player.check()
+        self.player.check_win()
         self.level.run()
         
         # draw objects
@@ -100,12 +154,16 @@ class Game:
         # move
         key = pg.key.get_pressed()
         if key[pg.K_UP]:
-            self.player.plr.left -= 20
-            self.player.plr.right -= 20
-            r = 0
+            self.level.ln -= 1
+            time.sleep(0.15)
+        if key[pg.K_DOWN]:
+            self.level.ln += 1
+            time.sleep(0.15)
+        
             
         if key[pg.K_LEFT]:
-            pass
+            self.player.plr.left -= 20
+            self.player.plr.top -= 20
         if key[pg.K_RIGHT]:
             pass
         if key[pg.K_SPACE]:
